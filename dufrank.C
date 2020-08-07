c4d814e82fbc3bd2d826cdaef456f46b8e8efaf7
miller86 <miller86@llnl.gov>
2020-08-07 14:54:38 -0700

    add dufort-frankel

--- dufrank.C
+++ dufrank.C
@@ -1,22 +1,21 @@
 #include "heat.H"
 
 bool                        // false if unstable, true otherwise
-update_solution_ftcs(
+update_solution_dufrank(
     int n,                  // number of samples
     Number *uk,             // new array of u(x,k) to compute/return
     Number const *uk1,      // array u(x,k-1) computed @ -1 time index ago
+    Number const *uk2,      // array u(x,k-2) computed @ -2 time index ago
     Number alpha,           // thermal diffusivity
     Number dx, Number dt,   // spacing in space, x, and time, t.
     Number bc0, Number bc1) // boundary conditions @ x=0 & x=Lx
 {
     Number r = alpha * dt / (dx * dx);
-
-    // sanity check for stability
-    if (r > 0.5) return false; 
+    Number q = 1 / (1+r);
 
     // FTCS update algorithm
     for (int i = 1; i < n-1; i++)
-        uk[i] = r*uk1[i+1] + (1-2*r)*uk1[i] + r*uk1[i-1];
+        uk[i] = q * (1-r) * uk2[i] + q * r * (uk1[i+1] + uk1[i-1]);
 
     // enforce boundary conditions
     uk[0  ] = bc0;
--- heat.C
+++ heat.C
@@ -26,7 +26,8 @@ Number min_change = Number(1e-8*1e-8);
 
 // Various arrays of numerical data
 Number *curr           = 0; // current solution
-Number *last           = 0; // last solution
+Number *back1          = 0; // solution back 1 step
+Number *back2          = 0; // solution back 2 steps
 Number *exact          = 0; // exact solution (when available)
 Number *change_history = 0; // solution l2norm change history
 Number *error_history  = 0; // solution error history (when available)
@@ -63,20 +64,26 @@ compute_exact_solution(int n, Number *a, Number dx, char const *ic,
 
 extern bool
 update_solution_ftcs(int n,
-    Number *curr, Number const *last,
+    Number *curr, Number const *back1,
     Number alpha, Number dx, Number dt,
     Number bc_0, Number bc_1);
 
 extern bool
 update_solution_upwind15(int n,
-    Number *curr, Number const *last,
+    Number *curr, Number const *back1,
     Number alpha, Number dx, Number dt,
     Number bc_0, Number bc_1);
 
 extern bool
 update_solution_crankn(int n,
-    Number *curr, Number const *last,
+    Number *curr, Number const *back1,
     Number const *cn_Amat,
+    Number bc_0, Number bc_1);
+
+extern bool
+update_solution_dufrank(int n, Number *curr,
+    Number const *back1, Number const *back2,
+    Number alpha, Number dx, Number dt,
     Number bc_0, Number bc_1);
 
 static void
@@ -86,8 +93,8 @@ initialize(void)
     Nt = (int) (maxt/dt);
     dx = lenx/(Nx-1);
 
-    curr = new Number[Nx]();
-    last = new Number[Nx]();
+    curr  = new Number[Nx]();
+    back1 = new Number[Nx]();
     if (save)
     {
         exact = new Number[Nx]();
@@ -97,6 +104,7 @@ initialize(void)
 
     assert(strncmp(alg, "ftcs", 4)==0 ||
            strncmp(alg, "upwind15", 8)==0 ||
+           strncmp(alg, "dufrank", 7)==0 ||
            strncmp(alg, "crankn", 6)==0);
 
 #ifdef HAVE_FEENABLEEXCEPT
@@ -106,8 +114,11 @@ initialize(void)
     if (!strncmp(alg, "crankn", 6))
         initialize_crankn(Nx, alpha, dx, dt, &cn_Amat);
 
+    if (!strncmp(alg, "dufrank", 7))
+        back2 = new Number[Nx]();
+
     /* Initial condition */
-    set_initial_condition(Nx, last, dx, ic);
+    set_initial_condition(Nx, back1, dx, ic);
 }
 
 int finalize(int ti, Number maxt, Number change)
@@ -128,7 +139,8 @@ int finalize(int ti, Number maxt, Number change)
     }
 
     delete [] curr;
-    delete [] last;
+    delete [] back1;
+    if (back2) delete [] back2;
     if (exact) delete [] exact;
     if (change_history) delete [] change_history;
     if (error_history) delete [] error_history;
@@ -144,11 +156,13 @@ static bool
 update_solution()
 {
     if (!strcmp(alg, "ftcs"))
-        return update_solution_ftcs(Nx, curr, last, alpha, dx, dt, bc0, bc1);
+        return update_solution_ftcs(Nx, curr, back1, alpha, dx, dt, bc0, bc1);
     else if (!strcmp(alg, "upwind15"))
-        return update_solution_upwind15(Nx, curr, last, alpha, dx, dt, bc0, bc1);
+        return update_solution_upwind15(Nx, curr, back1, alpha, dx, dt, bc0, bc1);
     else if (!strcmp(alg, "crankn"))
-        return update_solution_crankn(Nx, curr, last, cn_Amat, bc0, bc1);
+        return update_solution_crankn(Nx, curr, back1, cn_Amat, bc0, bc1);
+    else if (!strcmp(alg, "dufrank"))
+        return update_solution_dufrank(Nx, curr, back1, back2, alpha, dx, dt, bc0, bc1);
     return false;
 }
 
@@ -167,7 +181,7 @@ update_output_files(int ti)
     if (ti>0 && savi && ti%savi==0)
         write_array(ti, Nx, dx, curr);
 
-    change = l2_norm(Nx, curr, last);
+    change = l2_norm(Nx, curr, back1);
     if (save)
     {
         change_history[ti] = change;
@@ -213,8 +227,11 @@ int main(int argc, char **argv)
         if (outi && ti%outi==0)
             printf("Iteration %04d: last change l2=%g\n", ti, (double) change);
 
-        // Copy current solution to last
-        copy(Nx, last, curr);
+        // Copy current solution to backi
+        if (back2)
+            copy(Nx, back2, back1);
+        copy(Nx, back1, curr);
+
     }
 
     // Delete storage and output final results
